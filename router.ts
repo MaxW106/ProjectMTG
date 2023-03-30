164ed12e65671490da5398b480d24421d32dcdb2
Max Wiercx <max.wiercx@student.ap.be>
2023-03-30 13:02:54 +0200

    create 404 page create footer and update header redirect / to /home

--- router.ts
+++ router.ts
@@ -1,11 +1,23 @@
 import express from "express";
 const app = express();
 
+let pages_logged_in = ["home", "decks", "drawtest"];
+let pages_not_logged_in = ["home"];
+
 app.set("port", 3000);
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render("home.ejs");
+    res.redirect("home");
+});
+
+app.get("/home", (req, res) => {
+    res.render("home");
+});
+
+app.get("/*", (req, res) => {
+    res.status(404);
+    res.render("404", { pages: pages_logged_in });
 });
 
 app.listen(app.get("port"), () => {
