a1db8ca4fb172a5627781b9b65ccc337a877fcee
Max Wiercx <max.wiercx@student.ap.be>
2023-04-08 22:07:26 +0200

    add static folder to express add stylesheets link to head partial

--- router.ts
+++ router.ts
@@ -1,4 +1,5 @@
 import express from "express";
+const path = require("path");
 const app = express();
 
 let pages_logged_in = ["home", "decks", "drawtest"];
@@ -7,29 +8,31 @@ let pages_not_logged_in = ["home"];
 app.set("port", 3000);
 app.set("view engine", "ejs");
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
-    res.redirect("home");
+	res.redirect("home");
 });
 
 app.get("/home", (req, res) => {
-    res.render("home", { pages: pages_logged_in });
+	res.render("home", { pages: pages_logged_in });
 });
 
 app.get("/decks", (req, res) => {
-    res.render("decks", { pages: pages_logged_in });
+	res.render("decks", { pages: pages_logged_in });
 });
 
 app.get("/drawtest", (req, res) => {
-    res.render("drawtest", { pages: pages_logged_in });
+	res.render("drawtest", { pages: pages_logged_in });
 });
 
 app.get("/*", (req, res) => {
-    res.status(404);
-    res.render("404", { pages: pages_logged_in });
+	res.status(404);
+	res.render("404", { pages: pages_logged_in });
 });
 
 app.listen(app.get("port"), () => {
-    console.log(
-        `Web application running at http://localhost:${app.get("port")}`
-    );
+	console.log(
+		`Web application running at http://localhost:${app.get("port")}`
+	);
 });
