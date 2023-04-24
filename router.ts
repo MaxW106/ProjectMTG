b357c3ebcaf80c0a6776853f8eae22aedc16dc58
Max Wiercx <max.wiercx@student.ap.be>
2023-04-24 13:52:13 +0200

    make semi functional deck view page

--- router.ts
+++ router.ts
@@ -12,42 +12,48 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.render("landingpage");
+	res.render("landingpage");
 });
 
 app.get("/home", (req, res) => {
-  res.type("html");
-  let searchString = req.query.searchString as string;
-  let cards: any[] = [];
-  if (searchString !== "") {
-    cards = db.cards.filter((card, index, array) => {
-      return card.name.toLowerCase().includes(searchString);
-    });
-  }
-  if (cards.length == 0) {
-    cards = db.cards.slice(0, 10);
-  }
+	res.type("html");
+	let searchString = req.query.searchString as string;
+	let cards: any[] = [];
+	if (searchString !== "") {
+		cards = db.cards.filter((card, index, array) => {
+			return card.name.toLowerCase().includes(searchString);
+		});
+	}
+	if (cards.length == 0) {
+		cards = db.cards.slice(0, 10);
+	}
 
-  res.render("home", { pages: pages_logged_in, cards: cards });
+	res.render("home", { pages: pages_logged_in, cards: cards });
 });
 
 app.get("/decks", (req, res) => {
-  res.render("decks", { pages: pages_logged_in });
+	res.render("decks", { pages: pages_logged_in });
 });
 
 app.get("/drawtest", (req, res) => {
-  res.render("drawtest", { pages: pages_logged_in });
+	res.render("drawtest", { pages: pages_logged_in });
 });
 
 app.get("/login", (req, res) => {
-  res.render("login", { pages: pages_logged_in });
+	res.render("login", { pages: pages_logged_in });
+});
+
+app.get("/deck", (req, res) => {
+	res.render("deck_view", { pages: pages_logged_in, deck: db.decks[0] });
 });
 
 app.get("/*", (req, res) => {
-  res.status(404);
-  res.render("404", { pages: pages_logged_in });
+	res.status(404);
+	res.render("404", { pages: pages_logged_in });
 });
 
 app.listen(app.get("port"), () => {
-  console.log(`Web application running at http://localhost:${app.get("port")}`);
+	console.log(
+		`Web application running at http://localhost:${app.get("port")}`
+	);
 });
