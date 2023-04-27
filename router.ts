32ceae098b19bc6e52ddda30cb751a6e6217d5e6
Artjom Kuzmenko <S140459@ap.be>
2023-04-27 11:59:14 +0200

    l

--- router.ts
+++ router.ts
@@ -3,6 +3,7 @@ const path = require("path");
 const app = express();
 
 import db from "./db.json";
+import { name } from "ejs";
 let pages_logged_in = ["home", "decks", "drawtest", "login"];
 let pages_not_logged_in = ["home", "login"];
 
@@ -12,55 +13,55 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-	res.render("landingpage");
-	res.render("typescript/errorBox")
+  res.render("landingpage");
+  res.render("javascript/errorBox");
 });
 
 app.get("/home", (req, res) => {
-	res.type("html");
-	let searchString = req.query.searchString as string;
-	if (!searchString) searchString = "";
-	let cards: any[] = [];
-	if (searchString !== "") {
-		cards = db.cards.filter((card, index, array) => {
-			return card.name.toLowerCase().includes(searchString.toLowerCase());
-		});
-	}
-	if (cards.length == 0) {
-		cards = db.cards.slice(0, 10);
-	}
+  res.type("html");
+  let searchString = req.query.searchString as string;
+  if (!searchString) searchString = "";
+  let cards: any[] = [];
+  if (searchString !== "") {
+    cards = db.cards.filter((card, index, array) => {
+      return card.name.toLowerCase().includes(searchString.toLowerCase());
+    });
+  }
+  if (cards.length == 0) {
+    cards = db.cards.slice(0, 10);
+  }
 
-	res.render("home", {
-		pages: pages_logged_in,
-		cards: cards,
-		searchString: searchString,
-	});
+  res.render("home", {
+    pages: pages_logged_in,
+    cards: cards,
+    searchString: searchString,
+  });
 });
 
-
 app.get("/decks", (req, res) => {
-	res.render("decks", { pages: pages_logged_in });
+  res.render("decks", { pages: pages_logged_in });
 });
 
 app.get("/drawtest", (req, res) => {
-	res.render("drawtest", { pages: pages_logged_in });
+  res.render("drawtest", { pages: pages_logged_in });
 });
 
 app.get("/login", (req, res) => {
-	res.render("login", { pages: pages_logged_in });
+  res.render("login", { pages: pages_logged_in });
 });
 
 app.get("/deck", (req, res) => {
-	res.render("deck_view", { pages: pages_logged_in, deck: db.decks[0] });
+  let number = req.query.number as string;
+  if (!number) number = "0";
+  let i = parseInt(number);
+  res.render("deck_view", { pages: pages_logged_in, deck: db.decks[i] });
 });
 
 app.get("/*", (req, res) => {
-	res.status(404);
-	res.render("404", { pages: pages_logged_in });
+  res.status(404);
+  res.render("404", { pages: pages_logged_in });
 });
 
 app.listen(app.get("port"), () => {
-	console.log(
-		`Web application running at http://localhost:${app.get("port")}`
-	);
+  console.log(`Web application running at http://localhost:${app.get("port")}`);
 });
