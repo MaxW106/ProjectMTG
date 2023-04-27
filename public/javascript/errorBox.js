52a0784dd9696156d8cd17f4480d57a75663c93c
Artjom Kuzmenko <S140459@ap.be>
2023-04-27 12:24:46 +0200

    lol

--- public/javascript/errorBox.js
+++ public/javascript/errorBox.js
@@ -37,5 +37,29 @@ class ErrorBox {
 	}
   }
 
+/*
+// Definieer een functie die een error box maakt
+function showErrorBox(message) {
+    const errorBox = document.createElement("div");
+    errorBox.classList.add("error-box");
+    errorBox.textContent = message;
+    document.body.appendChild(errorBox);
+}
+
+console.log("test test");
+
+// Roep de functie aan wanneer er op een knop wordt gedrukt
+const a = document.querySelectorAll("a");
+a.forEach((element) => {
+    element.addEventListener("click", () => {
+        // Simuleer een error
+        const errorMessage = "Geen toegang tot deze pagina.";
+
+        // Laat de error box zien
+        showErrorBox(errorMessage);
+    });
+});
+*/
+
 
   
