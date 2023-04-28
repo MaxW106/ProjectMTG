e9224700c83a683b371f2393752589607d0c0763
Max Wiercx <max.wiercx@student.ap.be>
2023-04-28 13:47:51 +0200

    fix error popup when press mtg link

--- public/javascript/errorBox.js
+++ public/javascript/errorBox.js
@@ -1,4 +1,4 @@
-let errorbox = document.getElementById("error-box")
+let errorbox = document.getElementById("error-box");
 /*
 class ErrorBox {
 	constructor(className) {
@@ -37,15 +37,13 @@ class ErrorBox {
 	}
   }*/
 
-
 // Definieer een functie die een error box maakt
 function showErrorBox(message) {
-    const errorBox = document.createElement("div");
-    /* errorBox.classList.add("error-box"); */
-    errorBox.textContent = message;
-	console.log(errorBox)
-    document.body.appendChild(errorBox);
-
+	const errorBox = document.createElement("div");
+	/* errorBox.classList.add("error-box"); */
+	errorBox.textContent = message;
+	console.log(errorBox);
+	document.body.appendChild(errorBox);
 }
 
 console.log("test test");
@@ -53,14 +51,12 @@ console.log("test test");
 // Roep de functie aan wanneer er op een knop wordt gedrukt
 const a = document.querySelectorAll("a");
 a.forEach((element) => {
-    element.addEventListener("click", () => {
-        // Simuleer een error
-		errorbox.style.visibility ="visible"
-        // Laat de error box zien
-        showErrorBox(errorMessage);
-    });
+	if (element.className == "a") {
+		element.addEventListener("click", () => {
+			// Simuleer een error
+			errorbox.style.visibility = "visible";
+			// Laat de error box zien
+			showErrorBox(errorMessage);
+		});
+	}
 });
-
-
-
-  
