d7ef2c8e27e512f5530ad0b85bf01372c1fe51f4
Artjom Kuzmenko <S140459@ap.be>
2023-05-04 12:53:35 +0200

    einde les

--- public/javascript/passwordChecker.js
+++ public/javascript/passwordChecker.js
@@ -1,6 +1,24 @@
 let password = document.getElementById("password");
 let passwordChecker = document.getElementById("password-verification");
 let registerButton = document.getElementById("register-button");
+let formregister = document.getElementById("form-register");
 
+function processForm(e){
+    if(e.preventDefault) e.preventDefault();
+    return false;
+}
+if(formregister.attachEvent){
+    formregister.attachEvent("submit", processForm);
+
+}else{
+    formregister.addEventListener("submit", processForm);
+}
+/* checken wachtwoord */
+
+registerButton.addEventListener("click", function (){
+    if(passwordChecker != password){
+        print("Your password is not the same!")
+    };
+});
 
 
